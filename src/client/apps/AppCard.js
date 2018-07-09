59c8f6b65851ccdbcab7320653ba81140f015585
Your Name <you@example.com>
2018-07-10 05:01:49 +0800

    123

---
+++ src/client/apps/AppCard.js
@@ -0,0 +1,58 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
+
+const styles = {
+  card: {
+    maxWidth: 345,
+  },
+  media: {
+    height: 0,
+    paddingTop: '56.25%', // 16:9
+  },
+};
+
+class AppCard extends React.Component {
+  constructor(props){
+    super(props);
+
+
+  }
+  componentDidMount(){
+
+  }
+  render(){
+    const { classes } = this.props;
+      return (<div>
+      <Card className={classes.card}>
+        <CardMedia
+          className={classes.media}
+          title={"title"}
+        />
+        <CardContent>
+
+          <Typography variant="title">
+          title
+          </Typography>
+          <Typography gutterBottom variant="subheading">
+            actor
+          </Typography>
+        </CardContent>
+
+      </Card>
+    </div>)
+  }
+
+}
+
+Creeper.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles)(AppCard);
