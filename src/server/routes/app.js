9236aa5ebd6537a7393461f446125a853b59a98d
韩佩彧 <Vivien@hanpeiyudeMacBook-Air.local>
2018-07-11 22:17:01 +0800

    获取openid重构

--- src/server/routes/app.js
+++ src/server/routes/app.js
@@ -5,31 +5,35 @@ const  approute = new Router();
 
 const app_secrect = "9f22e4512d30fd774d93defa85c3282b";
 const app_id = "wx9ca3272fed4926ba";
-approute.get('/app/:appname/:from_url', async ( ctx )=>{
-    let from_url = ctx.params.from_url ? ctx.params.from_url : "";
-    if(!ctx.query.code){
-      await ctx.render('app', {
-        appname: ctx.params.appname,
-        openid: null,
-        from_url
-      })
-    }else{
-      Axios.get(`https://api.weixin.qq.com/sns/oauth2/access_token?appid=${app_id}&secret=${app_secrect}&code=${ctx.query.code}&grant_type=authorization_code`)
-      .then(res=>{
-        console.log(res.data);
-        ctx.render('app', {
-          appname: ctx.params.appname,
-          openid: res.data.openid,
-          from_url,
-        })
-      }).catch(err=>{
-        console.log(err);
+approute.get('/app/getopenid/:from_url', async ( ctx )=>{
+    let from_url = ctx.params.from_url ? ctx.params.from_url : "http://test2.10000cars.cn";
+    await ctx.render('getopenid', {
+      from_url
+    })
 
-      })
+})
+.get('/app/goto/:appname', async (ctx) => {
+  if(ctx.query.openid){
+    console.log(openid);
+  }
+  await ctx.render('app', {
+    openid,
+    appname: ctx.params.appname
+  })
+})
+.get("/app/getopenid/:from_url/back", async (ctx) => {
 
-
-    }
-
+  Axios.get(`https://api.weixin.qq.com/sns/oauth2/access_token?appid=${app_id}&secret=${app_secrect}&code=${ctx.query.code}&grant_type=authorization_code`)
+  .then(res=>{
+    console.log(res.data);
+    ctx.render('getopenid_back', {
+      openid: res.data.openid,
+      from_url,
+    })
+  }).catch(err=>{
+    console.log(err);
+    ctx.redirect(ctx.params.from_url);
+  })
 })
 
 export default approute;
