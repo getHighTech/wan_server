a121e85ec471a0f31be39eda2948e9957a24eb87
韩佩彧 <Vivien@hanpeiyudeMacBook-Air.local>
2018-07-31 18:05:52 +0800

    from hanpeiyu

--- test/units/mobile_sms.js
+++ test/units/mobile_sms.js
@@ -1,11 +1,11 @@
 import chai from 'chai';
-import MobileSMS from '../../src/server/models/MobileSMS.js';
+// import MobileSMS from '../../src/server/models/MobileSMS.js';
 
 
 let expect = chai.expect;
 
 describe('MobileSMS类短信验证方法测试', function(){
-    //由于运营商限制，此类测试不可频繁调用
+    // 由于运营商限制，此类测试不可频繁调用
 
     // it('测试getMobileSMS方法，发送短信给18820965455', (done)=>{
     //     MobileSMS.getMobileSMS("18820965455").then(rlt=>{
@@ -15,12 +15,14 @@ describe('MobileSMS类短信验证方法测试', function(){
     // });
     // it("测试validSMS方法, 返回验证成功，并且成功删除了这条揭露", (done)=>{
     //     MobileSMS.validSMS("18820965455").then(rlt1=>{
+    //         console.log(rlt1);
+            
     //         expect(rlt1).to.exist;
     //         MobileSMS.model.findOne({mobile: "188209654555"}).then(
     //             rlt2 => {
     //                 console.log(rlt2);
                     
-    //                 expect(rlt).to.be(false);
+    //                 expect(rlt2).to.not.exist;
     //                 done();
 
     //             }
