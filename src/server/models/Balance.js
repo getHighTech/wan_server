82668f93f0d33c7abd19a93cc31969fc7560aace
韩佩彧 <Vivien@hanpeiyudeMacBook-Air.local>
2018-08-01 16:31:39 +0800

    完成分享佣金

--- src/server/models/Balance.js
+++ src/server/models/Balance.js
@@ -1,8 +1,24 @@
+import WanModel from "../core/model.js";
 import mongoose from 'mongoose';
-const Schema = mongoose.Schema
 
-const BalanceSchema = new Schema(
+class Balance extends WanModel {
+    constructor(props){
+        super(props);
+        this.collection = "balances";
+    }
+   
+}
+
+Balance.setScheme(
+    {
+      "amount": Number,
+      "createdAt" : { type: Date, default: Date.now },
+      "userId": { type: mongoose.Schema.ObjectId, ref: 'users' },
+      
+
+    },
+    "Balance", "balances"
 
 )
 
-export default mongoose.model('Balance', BalanceSchema,);
+export default  Balance;
--- src/server/models/OrderDeal.js
+++ src/server/models/OrderDeal.js
@@ -2,6 +2,9 @@ import WanModel from "../core/model.js";
 import Order from "./Order.js";
 import ShopOrder from "./ShopOrder.js";
 import AgencyRelation from "./AgencyRelation.js";
+import User from "./User.js";
+import Shop from "./Shop.js";
+import BalanceIncome from "./BalanceIncome.js";
 
 class OrderDeal extends WanModel {
     constructor(props){
@@ -49,8 +52,6 @@ class OrderDeal extends WanModel {
                 status: "paid"
             });
             console.log("获取店铺的每个商品的佣金");
-            let superAgencyProfits = [];//一层佣金
-            let superAgencyProfits = [];//二层佣金
             let productsTamp = [];
             shopOrders.forEach(async shopOrder => {
                 shopOrder.products.forEach(product => {
@@ -58,22 +59,75 @@ class OrderDeal extends WanModel {
                 })
             })
             productsTamp.forEach(product => {
+                let agencyProfit = 0;
+                let superAgencyProfit = 0;
                 if(product.agencyLevelPrices[0]){
-                    agencyProfits.push(product.agencyLevelPrices[0])
+                    agencyProfit = product.agencyLevelPrices[0];
                 }
                 if(product.agencyLevelPrices[1]){
-                    superAgencyProfits.push(product.agencyLevelPrices[1]);
+                    superAgencyProfit = product.agencyLevelPrices[1];
                 }
                 console.log("更改代理关系");
-                let rel = AgencyRelation.find({userId: order.userId}).populate({
-                    path: 'shopId',
-                    select: '_id userId',
-                    model: 'shops',
-                    }).update({userId: order.userId}, {
-                        shopId
-                    });
+                let agency = await AgencyRelation.findOne({userId: order.userId});
+                console.log(agency);
+                
+                await AgencyRelation.update({_id: agency._id}, {
+                    shopId: shopOrder.shopId
+                })
+
+                console.log("获取产品的店铺");
+                let shop = await Shop.model.findOne({_id: shopOrder.shopId});
+                let userId = await shop.acl.own.users;
+                let shopOwner = await User.model.findOne({_id: userId});
+                console.log("================================");
+                console.log("给店铺拥有者佣金");
+                let balance  = await   Balance.model.findOne({userId: shopOwner._id});
+                await BalanceIncome.create({
+                    "amount": agencyProfit*shopOrder.productCounts[product._id],
+                    "userId": shopOrder.userId,
+                    "reasonType": "agencyGive",
+                    "agency": shopOwner._id,
+                    "text": "店铺代理营收",
+                    "productId": product._id,
+                    "productCounts": shopOrder.productCounts[product._id],
+                    "balanceId": balance._id,
+                });
+                let balanceAmount = balance.amount + parseInt(agencyProfit)*shopOrder.productCounts[product._id];
+                let balance_update = await Balance.model.update({_id: balance._id}, {
+                    amount: balanceAmount
+                })
+                console.log(balance_update);
                 
                 console.log("================================");
+                console.log("给平台拥有者佣金");
+                let SshopId = agency.SshopId;
+                if(!SshopId){
+                    return false;
+                }
+                let Sshop = await Shop.model.findOne({_id: SshopId});
+                let SuserId = await Sshop.acl.own.users;
+                let SshopOwner = await User.model.findById(SuserId);
+                let Sbalance = await Balance.model.findOne({userId: SshopOwner._id});
+                await BalanceIncome.create({
+                    "amount": superAgencyProfit*shopOrder.productCounts[product._id],
+                    "userId": shopOrder.userId,
+                    "reasonType": "agencyGive",
+                    "agency": SshopOwner._id,
+                    "text": "店铺代理营收",
+                    "productId": product._id,
+                    "productCounts": shopOrder.productCounts[product._id],
+                    "balanceId": Sbalance._id,
+                });
+                let SbalanceAmount = Sbalance.amount + parseInt(agencyProfit)*shopOrder.productCounts[product._id];
+                let Sbalance_update = await Balance.model.update({_id: balance._id}, {
+                    amount: SbalanceAmount
+                })
+                console.log(Sbalance_update);
+                console.log("================================");
+
+
+
+
 
             });
             
@@ -83,14 +137,6 @@ class OrderDeal extends WanModel {
             console.log("================================");
             console.log("================================");
             console.log("================================");
-
-
-
-            
-            console.log("处理每一个订单");
-
-
-
             console.log("================================");
             await this.model.update({_id: deal._id},{
                 status: "done"
