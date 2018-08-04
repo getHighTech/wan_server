5ab1efcc412c9600115c46007c34489132e63129
Hikaru <shindouhiro@gmail.com>
2018-08-05 00:20:32 +0800

    api/info添加code参数

--- src/server/routes/api/api.js
+++ src/server/routes/api/api.js
@@ -10,7 +10,8 @@ import rp from 'request-promise'
 import moment from "moment"
 const  ApiRoute = new Router();
 ApiRoute.get('/api/info', async (ctx) => {
- let appid = 'wxb202efbc9c76046a'; //公众号appid
+  let code = ctx.query.code; 
+  let appid = 'wxb202efbc9c76046a'; //公众号appid
   let secret = 'b8d396c379cc2b2277c4f1c062365f3a';
   let fetchWechatUserInfo = `https://api.weixin.qq.com/sns/oauth2/access_token?appid=${appid}&secret=${secret}&code=${code}&grant_type=authorization_code `;
   let options = {
