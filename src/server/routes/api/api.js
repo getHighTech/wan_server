9a30c492ed06286d9e20479f2e47a305b4c0c7af
Hikaru <shindouhiro@gmail.com>
2018-08-04 23:47:45 +0800

    添加api/wechat/info

--- src/server/routes/api/api.js
+++ src/server/routes/api/api.js
@@ -6,11 +6,33 @@ import Balance from '../../models/Balance.js';
 import BalanceCharge from '../../models/BalanceCharge.js';
 import Agency from '../../models/Agency.js';
 // import User from '../../models/User.js'
-
+const rp = require('    ')
 import moment from "moment"
 const  ApiRoute = new Router();
+ApiRoute.get('/api/wechat/info', async (ctx) => {
+ let appid = 'wx0564668ed5671740'; //公众号appid
+  let secret = '02938e071aae51a7b59b7fe6f627a681';
+  let fetchWechatUserInfo = `https://api.weixin.qq.com/sns/oauth2/access_token?appid=${appid}&secret=${secret}&code=${code}&grant_type=authorization_code `;
+  let options = {
+    method: 'GET',
+    uri: fetchWechatUserInfo,
+    json: true
+  }
+  let userInfo = await rp(options);
+  console.log(userInfo)
+  if(userInfo.errcode){
+    throw new Error('fetch userInfo failure, please check the params')
+  }
+  let {openid, access_token, refresh_token} = userInfo
 
-  
+  let fetchWechatUserDetailInfoUrl = `https://api.weixin.qq.com/sns/userinfo?access_token=${access_token}&openid=${openid}&lang=zh_CN `;
+  let userDetailInfo = await rp({method:'GET', uri: fetchWechatUserDetailInfoUrl, json:true })
+  userInfo = Object.assign({}, userInfo, userDetailInfo)
+  console.log(userInfo)
+  ctx.body = {
+      userInfo
+  }
+})
 
 ApiRoute.get('/api/order/status', async ( ctx )=>{
 //这是临时的借口
