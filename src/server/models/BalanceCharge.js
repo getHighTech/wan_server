09eb095c4bcfab31533a6dc0cd103510129a85aa
Hikaru <shindouhiro@gmail.com>
2018-08-13 10:54:24 +0800

    修改佣金

--- src/server/models/BalanceCharge.js
+++ src/server/models/BalanceCharge.js
@@ -2,7 +2,9 @@ import mongoose from 'mongoose';
 const Schema = mongoose.Schema
 
 const BalanceChargeSchema = new Schema(
-
+    {
+        _id: String
+    }
 )
 
 export default mongoose.model(' BalanceCharge', BalanceChargeSchema,'balance_charges');
