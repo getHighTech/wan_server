57bb053f2bcdf2b3f006e7afd45dd3374ef12c59
Your Name <you@example.com>
2018-08-21 15:19:31 +0800

    ..

--- src/server/routes/api/api.js
+++ src/server/routes/api/api.js
@@ -2,13 +2,14 @@ import Router from 'koa-router';
 import { getOrderStatus } from '../../controllers/order.js';
 import { wechatAuth } from '../../controllers/wechat.js';
 import { getTeam  } from '../../controllers/myTeam.js';
+import {getProductSpec} from '../../controllers/productSpec.js'
 import { getShopProducts, porudctsWarehouse, sellingProduct } from '../../controllers/product.js';
 import { getBankcards } from '../../controllers/bankcard.js';
 import { getPeriodIncome } from '../../controllers/balanceIncome.js';
 import { loadMoney } from '../../controllers/balance.js';
 const  ApiRoute = new Router();
 ApiRoute.get('/api/info', wechatAuth)
-
+ApiRoute.get('/api/findAllSpecProductByProductId',getProductSpec)
 ApiRoute.get('/api/order/status',getOrderStatus)
 ApiRoute.get('/api/myteam',getTeam)
 ApiRoute.get('/api/products',getShopProducts)
