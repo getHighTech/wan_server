0c663454356c4c6cdbf1f4b40fa286d2070bf511
= <shindouhiro@gmail.com>
2018-09-06 14:06:30 +0800

    ‘订单分页修改’

--- src/server/controllers/order.js
+++ src/server/controllers/order.js
@@ -2,17 +2,19 @@ import mongoose from 'mongoose';
 import Order from '../models/Order.js';
 
 export const getOrderStatus = async(ctx) => {
-    try{
-        const { userId, status,appName } = ctx.query
-        const order = await Order.model.find({userId,status,appName}).skip(0).limit(10).sort({createdAt: -1})
+    // try{
+        const { userId, status,appName,page } = ctx.query
+        console.log(page)
+        let pagesize = 10;
+        const order = await Order.model.find({userId,status,appName}).skip(pagesize*(page-1)).limit(10).sort({createdAt: -1})
         ctx.body = {
             order,
         }
-    } 
-    catch (err) {
-        ctx.body = {
-            msg: 'fail'
-        }
-    }
+    // } 
+    // catch (err) {
+    //     ctx.body = {
+    //         msg: 'fail'
+    //     }
+    // }
 }
 
