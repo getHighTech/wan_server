da8f4a3a93ecb888008c8b7262e8b51228c45532
Your Name <you@example.com>
2018-09-07 16:00:33 +0800

    ..

--- src/server/controllers/wechat.js
+++ src/server/controllers/wechat.js
@@ -3,9 +3,6 @@ import axios from 'axios';
 import sha1  from 'sha1';
 import mongoose from 'mongoose';
 import WechatShare  from '../models/WechatShare.js';
-import cookies from 'cookies'
-import Koa from 'koa';
-const  App = new Koa();
 
 export  const wechatAuth = async(ctx) => {
   try{
@@ -43,10 +40,14 @@ export  const wechatAuth = async(ctx) => {
 }
 
 export const wechatShare = async(ctx) =>{
-  // let appid = 'wx387b34583841ec2d'; //公众号appid
+  // let appid = 'wx387b34583841ec2d'; //自己公众号appid
   // let secret = '5e4e086325a72ffd80bf179e6a22749b';
+  //
   let appid = 'wx0564668ed5671740'; //公众号appid
   let secret = '02938e071aae51a7b59b7fe6f627a681';
+
+  // let appid = 'wx412cc1c5e02a292e'; //测试公众号appid
+  // let secret = '2cd967050582d6256d8108281af8e8eb';
     //获取accessToken
     const {url} = ctx.query;
     console.log(url);
@@ -74,27 +75,27 @@ export const wechatShare = async(ctx) =>{
     var time2=new Date();
     var time3= time2.getTime()-time1.getTime();
     console.log('时间差为：'+time3/1000);
-        if (time3/1000>7000) {
-          console.log('如果时间超过7000秒，重新获取');
-          const  res = await axios.get(`https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid=${appid}&secret=${secret}`)
-          access_token= res.data.access_token;
-          const result = await axios.get(`https://api.weixin.qq.com/cgi-bin/ticket/getticket?access_token=${access_token}&type=jsapi`);
-          ticket = result.data.ticket
-          let id = first[0]._id;
-          const refalsh = await WechatShare.model.update({_id:id},{
-            $set:{
-              access_token:access_token,
-              ticket:ticket,
-              createdAt:new Date()
-            }
-          })
-        }
-        else {
-              console.log('此时缓存还存在');
-              access_token = first[0].access_token;
-              ticket= first[0].ticket;
-              console.log('此时的ticket是：'+ticket);
-        }
+          if (time3/1000>7000) {
+            console.log('如果时间超过7000秒，重新获取');
+            const  res = await axios.get(`https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid=${appid}&secret=${secret}`)
+            access_token= res.data.access_token;
+            const result = await axios.get(`https://api.weixin.qq.com/cgi-bin/ticket/getticket?access_token=${access_token}&type=jsapi`);
+            ticket = result.data.ticket
+            let id = first[0]._id;
+            const refalsh = await WechatShare.model.update({_id:id},{
+              $set:{
+                access_token:access_token,
+                ticket:ticket,
+                createdAt:new Date()
+              }
+            })
+          }
+          else {
+                console.log('此时缓存还存在');
+                access_token = first[0].access_token;
+                ticket= first[0].ticket;
+                console.log('此时的ticket是：'+ticket);
+          }
   }else {
     console.log('数据库没有存储');
     const  res = await axios.get(`https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid=${appid}&secret=${secret}`)
