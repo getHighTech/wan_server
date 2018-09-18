733d5ff895644da6ba47d8937a67404f958fcf97
Your Name <you@example.com>
2018-09-18 17:11:59 +0800

    修改buyg

--- src/server/controllers/order.js
+++ src/server/controllers/order.js
@@ -1,16 +1,20 @@
 import mongoose from 'mongoose';
 import Order from '../models/Order.js';
+import ShopOrder from '../models/ShopOrder.js';
 
 export const getOrderStatus = async(ctx) => {
     // try{
         const { userId, status,appName,page } = ctx.query
         console.log(page)
-        let pagesize = 10;
-        const order = await Order.model.find({userId,status,appName}).skip(pagesize*(page-1)).limit(10).sort({createdAt: -1})
+        // let pagesize = 10;
+        let number = Number(page)*10
+        // const order = await Order.model.find({userId,status,appName}).skip(pagesize*(page-1)).limit(10).sort({createdAt: -1})
+        const order = await Order.model.find({userId,status,appName}).limit(number).sort({createdAt: -1})
+        console.log(order.length);
         ctx.body = {
             order,
         }
-    // } 
+    // }
     // catch (err) {
     //     ctx.body = {
     //         msg: 'fail'
@@ -18,3 +22,29 @@ export const getOrderStatus = async(ctx) => {
     // }
 }
 
+export const getOrderStatusConfirmed = async(ctx) => {
+    // try{
+        const { userId,orderId,appName } = ctx.query
+        const orders = await Order.model.update({_id:orderId},{$set:{status:'cancel'}});
+        const shoporder=await ShopOrder.model.update({orderId:orderId},{$set:{status:'cancel'}});
+
+        if(!orders || orders.lenght === 0){
+          ctx.body = {
+            msg: 'fail'
+          }
+      }else{
+          const order =await Order.model.find({userId,status: "confirmed",appName:appName})
+          ctx.body = {
+              order
+          }
+      }
+
+
+
+    // }
+    // catch (err) {
+    //     ctx.body = {
+    //         msg: 'fail'
+    //     }
+    // }
+}
